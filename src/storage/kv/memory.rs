65981f26825daaba73242ab14798fed9bdbf938d
maxnilz <xianchao.yu.io@gmail.com>
2025-03-10 14:51:56 +0800

    update raw kv storage
    Signed-off-by: maxnilz <xianchao.yu.io@gmail.com>

--- src/storage/kv/memory.rs
+++ src/storage/kv/memory.rs
@@ -2,7 +2,7 @@ use std::collections::btree_map::Range;
 use std::collections::{BTreeMap, VecDeque};
 use std::ops::Bound;
 
-use super::{Key, ScanIterator, Storage};
+use super::{ScanIterator, Storage};
 use crate::error::Result;
 
 #[derive(Debug, Clone)]
@@ -21,31 +21,31 @@ impl Storage for Memory {
         Ok(())
     }
 
-    fn set(&mut self, key: Key, value: Vec<u8>) -> Result<()> {
-        self.data.insert(key.encode()?, value);
+    fn set(&mut self, key: &[u8], value: Vec<u8>) -> Result<()> {
+        let key = Vec::from(key);
+        self.data.insert(key, value);
         Ok(())
     }
 
-    fn get(&self, key: Key) -> Result<Option<Vec<u8>>> {
-        let key = key.encode()?;
-        let value = self.data.get(&key);
+    fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>> {
+        let value = self.data.get(key);
         Ok(value.cloned())
     }
 
-    fn scan(&self, range: (Bound<Key>, Bound<Key>)) -> Box<dyn ScanIterator<'_> + '_> {
-        let r = (range.0.map(|k| k.encode().unwrap()), range.1.map(|k| k.encode().unwrap()));
-        let result = self.data.range(r);
+    fn scan(&self, range: (Bound<Vec<u8>>, Bound<Vec<u8>>)) -> Box<dyn ScanIterator<'_> + '_> {
+        let result = self.data.range(range);
         Box::new(RangeIterator { it: result })
     }
 
-    fn remove(&mut self, key: Key) -> Result<Option<Vec<u8>>> {
-        let key = key.encode()?;
-        Ok(self.data.remove(&key))
+    fn remove(&mut self, key: &[u8]) -> Result<Option<Vec<u8>>> {
+        Ok(self.data.remove(key))
     }
 
-    fn remove_range(&mut self, range: (Bound<Key>, Bound<Key>)) -> Box<dyn ScanIterator<'_> + '_> {
-        let r = (range.0.map(|k| k.encode().unwrap()), range.1.map(|k| k.encode().unwrap()));
-        let keys = self.data.range(r).map(|(k, _)| k.clone()).collect::<Vec<_>>();
+    fn remove_range(
+        &mut self,
+        range: (Bound<Vec<u8>>, Bound<Vec<u8>>),
+    ) -> Box<dyn ScanIterator<'_> + '_> {
+        let keys = self.data.range(range).map(|(k, _)| k.clone()).collect::<Vec<_>>();
         let mut result: VecDeque<(Vec<u8>, Vec<u8>)> = VecDeque::new();
         for key in keys {
             let value = self.data.remove(&key);
@@ -126,18 +126,18 @@ mod tests {
         let mut m = Memory::new();
 
         // get missing key
-        assert_eq!(None, m.get(key!(b"a"))?);
+        assert_eq!(None, m.get(b"a")?);
 
         // set & get key
-        m.set(key!(b"a"), vec![1])?;
-        assert_eq!(Some(vec![1]), m.get(key!(b"a"))?);
+        m.set(b"a", vec![1])?;
+        assert_eq!(Some(vec![1]), m.get(b"a")?);
 
         // delete
-        let got = m.remove(key!(b"a"))?;
+        let got = m.remove(b"a")?;
         assert_eq!(Some(vec![1]), got);
 
         // get again
-        assert_eq!(None, m.get(key!(b"a"))?);
+        assert_eq!(None, m.get(b"a")?);
 
         Ok(())
     }
@@ -147,13 +147,13 @@ mod tests {
         let mut m = Memory::new();
 
         // set values
-        m.set(key!(b"apple"), vec![1])?;
-        m.set(key!(b"app"), vec![2])?;
-        m.set(key!(b"banala"), vec![3])?;
-        m.set(key!(b"ba"), vec![4])?;
-        m.set(key!(b"cat\x1F"), vec![5])?;
-        m.set(key!(b"cat\x1E"), vec![6])?;
-        m.set(key!(b"cat\x20"), vec![7])?;
+        m.set(b"apple", vec![1])?;
+        m.set(b"app", vec![2])?;
+        m.set(b"banala", vec![3])?;
+        m.set(b"ba", vec![4])?;
+        m.set(b"cat\x1F", vec![5])?;
+        m.set(b"cat\x1E", vec![6])?;
+        m.set(b"cat\x20", vec![7])?;
 
         // scan prefix
         let iter = m.scan_prefix(b"app");
@@ -166,10 +166,10 @@ mod tests {
         assert_eq!(vec![vec![6], vec![5], vec![7]], values);
 
         // scan prefix
-        m.set(key!(vec![1, 1, 1]), vec![8])?;
-        m.set(key!(vec![1, 1, 2]), vec![9])?;
-        m.set(key!(vec![1, 2, 1]), vec![10])?;
-        m.set(key!(vec![1, 2, 2]), vec![11])?;
+        m.set(&vec![1, 1, 1], vec![8])?;
+        m.set(&vec![1, 1, 2], vec![9])?;
+        m.set(&vec![1, 2, 1], vec![10])?;
+        m.set(&vec![1, 2, 2], vec![11])?;
 
         let iter = m.scan_prefix(&vec![1, 1]);
         let values = iter.map(|it| it.unwrap().1).collect::<Vec<_>>();
@@ -183,27 +183,27 @@ mod tests {
         let mut m = Memory::new();
 
         // set values
-        m.set(key!(b"a"), vec![1])?;
-        m.set(key!(b"b"), vec![2])?;
-        m.set(key!(b"c"), vec![3])?;
-        m.set(key!(b"d"), vec![4])?;
+        m.set(b"a", vec![1])?;
+        m.set(b"b", vec![2])?;
+        m.set(b"c", vec![3])?;
+        m.set(b"d", vec![4])?;
 
         // range scan
-        let start = Bound::Included(key!(b"a"));
-        let end = Bound::Excluded(key!(b"d"));
+        let start = Bound::Included(b"a".to_vec());
+        let end = Bound::Excluded(b"d".to_vec());
         let iter = m.scan((start, end));
         let values = iter.map(|it| it.unwrap().1).collect::<Vec<_>>();
         assert_eq!(vec![vec![1], vec![2], vec![3]], values);
 
         // set more values
-        m.set(key!(vec![1, 1, 1]), vec![8])?;
-        m.set(key!(vec![1, 1, 2]), vec![9])?;
-        m.set(key!(vec![1, 2, 1]), vec![10])?;
-        m.set(key!(vec![1, 2, 2]), vec![11])?;
+        m.set(&vec![1, 1, 1], vec![8])?;
+        m.set(&vec![1, 1, 2], vec![9])?;
+        m.set(&vec![1, 2, 1], vec![10])?;
+        m.set(&vec![1, 2, 2], vec![11])?;
 
         // range scan with half open range
-        let from = Bound::Included(key!(vec![1, 1]));
-        let to = Bound::Excluded(key!(vec![1, 255]));
+        let from = Bound::Included(vec![1, 1]);
+        let to = Bound::Excluded(vec![1, 255]);
         let iter = m.scan((from, to));
         let values = iter.map(|it| it.unwrap().1).collect::<Vec<_>>();
         assert_eq!(vec![vec![8], vec![9], vec![10], vec![11]], values);
@@ -211,8 +211,8 @@ mod tests {
         // range scan on [from, to] with the exactly matched key,
         // since the key `vec![1, 2, 2]` is included in the range,
         // the result should contain the value of `vec![1, 2, 2]`.
-        let from = Bound::Included(key!(vec![1, 1, 1]));
-        let to = Bound::Included(key!(vec![1, 2, 2]));
+        let from = Bound::Included(vec![1, 1, 1]);
+        let to = Bound::Included(vec![1, 2, 2]);
         let iter = m.scan((from, to));
         let values = iter.map(|it| it.unwrap().1).collect::<Vec<_>>();
         assert_eq!(vec![vec![8], vec![9], vec![10], vec![11]], values);
@@ -221,8 +221,8 @@ mod tests {
         // since the key `vec![1, 2]` is not an exact key in the storage,
         // the result should not contain any value greater that `vec![1, 2]`,
         // and vec[1, 2] itself.
-        let from = Bound::Included(key!(vec![1, 1]));
-        let to = Bound::Included(key!(vec![1, 2]));
+        let from = Bound::Included(vec![1, 1]);
+        let to = Bound::Included(vec![1, 2]);
         let iter = m.scan((from, to));
         let values = iter.map(|it| it.unwrap().1).collect::<Vec<_>>();
         assert_eq!(vec![vec![8], vec![9]], values);
